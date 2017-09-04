080b424d4702b64497f9ed0f6b70fca129681bf0
Tao Lei <taolei@csail.mit.edu>
2017-09-04 21:11:10 +0000

    aaabccc

--- drqa/rnn_reader.py
+++ drqa/rnn_reader.py
@@ -79,7 +79,6 @@ class RnnDocReader(nn.Module):
             rnn_type=self.RNN_TYPES[opt['rnn_type']],
             padding=opt['rnn_padding'],
             rnn_dropout_rate=opt['rnn_dropout_rate'],
-            bias = opt['bias'],
         )
 
         # RNN question encoder
@@ -93,7 +92,6 @@ class RnnDocReader(nn.Module):
             rnn_type=self.RNN_TYPES[opt['rnn_type']],
             padding=opt['rnn_padding'],
             rnn_dropout_rate=opt['rnn_dropout_rate'],
-            bias = opt['bias'],
         )
 
         # Output sizes of rnn encoders
